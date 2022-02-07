fe3e34cbaa8c163d208d0ad164a40db5a320b9a5
Agrain7 <purplekir@naver.com>
2022-02-07 16:31:46 +0900

    두번째저장 비밀정보보호

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const { User } = require("./models/User");
+
+const config = require("./config/key")
+
+//www-form
+app.use(express.urlencoded({extended: true}));
+
+//json type
+app.use(express.json());
+
 
 const mongoose= require('mongoose')
-mongoose.connect('mongodb+srv://agrain7:abcd1234@agrain7.avgzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
 
 }).then(() => console.log('MongoDB Connected..'))
   .catch(err => console.log('err',err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.post('/register', (req, res) => {
+
+  //회원 가입 시 받아온 정보를 데이터 베이스에 저장
+
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if(err) return res.json({ success:false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
